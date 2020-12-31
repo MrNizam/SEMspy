03bbbcd458a483a0b709d0eb5269954dbba8d57e
jwiedeman <joshua.f.wiedeman@gmail.com>
2020-12-31 12:55:52 -0800

    commit

--- background.js
+++ background.js
@@ -8,71 +8,37 @@
      */
 
 
-
-     
-    function getParameterByName( url, name ) {
-        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-        var regexS = "[\\?&]" + name + "=([^&#]*)";
-        var regex = new RegExp(regexS);
-        var results = regex.exec(url);
-
-        if ( results === null ) {
-            return '';
-        } else {
-            return decodeURIComponent(results[1].replace(/\+/g, " "));
-        }
-    }
-
     // This function is called onload in the popup code
     function getPageInfo(callback) 
     { 
         // Add the callback to the queue
         callbacks.push(callback); 
-
         // Injects the content script into the current page 
         chrome.tabs.executeScript(null, { file: "content_script.js" }); 
     }; 
 
+
     // Perform the callback when a request is received from the content script
-    chrome.extension.onRequest.addListener(function(request) 
+    chrome.extension.onRequest.addListener(function(request)
     { 
         // Get the first callback in the callbacks array
         // and remove it from the array
         var callback = callbacks.shift();
-
         // Call the callback function
         callback(request); 
     }); 
     
+
     chrome.webRequest.onBeforeSendHeaders.addListener(
       function(details) {
-       console.log(details) 
-        var referer, eventString, uacode;
-        var category, action, label, val;
-
-        for (var i = 0; i < details.requestHeaders.length; ++i) {
-          if (details.requestHeaders[i].name === 'User-Agent') {
-            details.requestHeaders.splice(i, 1);
-            break;
-          }
+        var requestData = {
+          method : details.post,
+          tabId : details.tabId,
+          url:details.url
         }
-
-        if(details.url.indexOf('google-analytics') > -1) {
-
-            referer = tabid = eventString = uacode = category = action = label = val = '<i>null</i>';
-
-            tabid = details.tabId;
-            category = getParameterByName(details.url, 'ec');
-            action = getParameterByName(details.url, 'ea');
-            label = getParameterByName(details.url, 'el');
-            val = getParameterByName(details.url, 'ev');
-            uacode = getParameterByName(details.url, 'tid');
-            referer = getParameterByName(details.url, 'dl');
-
-            eventObject.push([referer, uacode, category, action, label, val, tabid]);
-            if(eventObject.length > 25) eventObject.shift();
-        }
-       
+        console.log('###', requestData)
+        eventObject.push(requestData)
+            
         return {requestHeaders: details.requestHeaders};
       },
       {urls: ["<all_urls>"]},
--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+        var currentTab;
+        
+        
         // This callback function is called when the content script has been 
         // injected and returned its results
         function onPageInfo(o) 
@@ -11,42 +14,54 @@
         }
 
         function updateList() {
-            var bgeo = chrome.extension.getBackgroundPage().eventObject;
+            var requestData = chrome.extension.getBackgroundPage().eventObject; // capture of all requests
             var newhtml = '';
-            var dispURL = '';
-            console.log(bgeo, chrome.tabs.query(
-                {currentWindow: true, active : true},
-                function(tabArray){console.log(tabArray)}
-              ))
-            if(bgeo.length) {
-                var countUntil;
-                if(bgeo.length > 15) {
-                    countUntil = bgeo.length - 15;
-                } else {
-                    countUntil = -1;
-                }
-                for(var i = bgeo.length-1; i > countUntil; i--) {
-                    if(bgeo[i][0].length > 37) {
-                        dispURL = bgeo[i][0].substr(0, 32) + ' ...';
-                    } else {
-                        dispURL = bgeo[i][0];
-                    }
+            var requestDataForCurrentTab = requestData.filter(function(x){ return x.tabId === currentTab;}); // new array from all requests, but only requests sent by the current tab
+            console.log(requestDataForCurrentTab)
+
+
+            if(requestDataForCurrentTab.length ) {
+                for(var i = requestDataForCurrentTab.length-1; i > 0; i--) {
+                    // parse the request
+                    var requestUrl = requestDataForCurrentTab[i].url;
+                    console.log(requestUrl)
+
+    
+                    category = getParameterByName(requestUrl, 'ec');
+                    action = getParameterByName(requestUrl, 'ea');
+                    label = getParameterByName(requestUrl, 'el');
+                    val = getParameterByName(requestUrl, 'ev');
+                    uacode = getParameterByName(requestUrl, 'tid');
+                    referer = getParameterByName(requestUrl, 'dl');
+        
+        
+
+
+
+
+
+
+
+
+
+
                     newhtml += '<tr class="entry">';
-                    newhtml += '<td>' + bgeo[i][2] + '</td>';
-                        newhtml += '<td class="uastring">' + bgeo[i][1] + '</td>';
-                        newhtml += '<td>' + bgeo[i][2] + '</td>';
-                        newhtml += '<td>' + bgeo[i][3] + '</td>';
-                        newhtml += '<td>' + bgeo[i][4] + '</td>';
-                        newhtml += '<td>' + bgeo[i][5] + '</td>';
+                        newhtml += '<td>' + requestDataForCurrentTab[i].initiator + '</td>';
+                        newhtml += '<td class="uastring">' + requestDataForCurrentTab[i][1] + '</td>';
+                        newhtml += '<td>' + requestDataForCurrentTab[i][2] + '</td>';
+                        newhtml += '<td>' + requestDataForCurrentTab[i][3] + '</td>';
+                        newhtml += '<td>' + requestDataForCurrentTab[i][4] + '</td>';
+                        newhtml += '<td>' + requestDataForCurrentTab[i][5] + '</td>';
                     newhtml += '</tr>';
                 }
                 document.getElementById('event-list').innerHTML = newhtml;
             } else {
-                newhtml += '<p><i>No events recorded yet.</i></p>';
+                newhtml += '<p><b>No events recorded yet.</b></p>';
                 document.getElementById('event-list').innerHTML = newhtml;
             }
 
         }
+
 
         // Call the getPageInfo function in the background page, passing in 
         // our onPageInfo function as the callback
@@ -55,11 +70,33 @@
                 var table = document.getElementById('event-table');
                 var tableClasses = table.classList;
                 
-            
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                    // on popup load, query chrome to get the tabs ID, then save to currentID so we can filter all incoming requests to only store requests coming from the tab that the popup was opened on
+                    currentTab = tabs[0].id;
+                });
+
+
             updateList();
-            setInterval(updateList, 250);
+            setInterval(updateList, 500);
             document.getElementById('reset-list').onclick = function() {
                 chrome.extension.getBackgroundPage().eventObject = [];
                 updateList();
             };
         };
+
+    
+
+
+     
+    function getParameterByName( url, name ) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(url);
+
+        if ( results === null ) {
+            return '';
+        } else {
+            return decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+    }
